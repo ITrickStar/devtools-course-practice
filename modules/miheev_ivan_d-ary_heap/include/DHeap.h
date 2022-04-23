3c9e4231ae781f296031dea381df1d193036dce4
ITrickStar <mia_2112@mail.ru>
2022-04-23 15:33:50 +0300

    additional constructor and tests added

--- modules/miheev_ivan_d-ary_heap/include/DHeap.h
+++ modules/miheev_ivan_d-ary_heap/include/DHeap.h
@@ -2,20 +2,27 @@
 
 #pragma once
 
-// Structure of a min heap node
+#define INT_MAX 2147483647
+
+// Structure of a heap node
 struct DHeapNode {
   int val; // value of the node
-  DHeapNode(int _val) : val(_val) {} // Function to create a new minHeapNode
+  DHeapNode(int _val = INT_MAX) : val(_val) {} // Function to create a new minHeapNode
 };
 
 // minimal element in the root of the heap
 class DHeap {
  private:
-  int d; // Sets the d-ary of minHeap Heaparr
+  int d; // Sets the d-ary of the heap
 
  public:
   int size; // Number of existing heap nodes
-  DHeapNode** heaparr; // the array on which the heap is built
+  DHeapNode* heaparr; // the array on which the heap is built
 
-  DHeap() : d(2), size(0), heaparr(nullptr) {} // Default constructor
+  DHeap() : size(0), d(2), heaparr(nullptr) {}  // Default constructor
+  DHeap(int _size, int _d) : size(_size) , d(_d) {
+    if (d < 2) throw "d-ary of the heap can't be less then 2";
+    heaparr = new DHeapNode[size];
+  }
+  
 };
--- modules/miheev_ivan_d-ary_heap/test/test_heap.cpp
+++ modules/miheev_ivan_d-ary_heap/test/test_heap.cpp
@@ -4,7 +4,27 @@
 
 #include "include/DHeap.h"
 
-TEST(miheev_ivan_dheap,
-     can_create_dheap_with_default_param) {
+TEST(miheev_ivan_dheap, default_test) {
+  // Arrange & Act
+
+  // Assert
+  ASSERT_NO_THROW();
+}
+
+TEST(miheev_ivan_dheap, can_call_constructor_with_default_param) {
+  // Assert
   ASSERT_NO_THROW(DHeap());
 }
+
+TEST(miheev_ivan_dheap, can_call_constructor_with_input_param) {
+  // Assert
+  ASSERT_NO_THROW(DHeap(5, 2));
+}
+
+// Must be false
+//TEST(miheev_ivan_dheap, can_create_heap_with_wrong_dary) {
+//  // Assert
+//  ASSERT_NO_THROW(DHeap(5, -2));
+//}
+
+
